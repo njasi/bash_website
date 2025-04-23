3b37ecbb3b2f90c10c3891e7937e73c5b6fa8c25
njasi <nick@jasinski3.com>
2025-04-23 15:14:05 -0600

    small updates

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ Welcome to (class=yellow)(Nick Jasinski's) Website
 
   * (class=green)(Github):         [https://github.com/njasi](https://github.com/njasi)
   * (class=green)(Linkedin):       [https://www.linkedin.com/in/njasi](https://www.linkedin.com/in/njasi)
-  * (class=green)(Email):          [nick@jasinski3.com](mailto:nick@jasinski3.com)
+  * (class=green)(Email):          [contact@njasi.com](mailto:contact@njasi.com)
 
 (class=cyan bold)(About Me):
 Hi! I'm Nick, a software developer with a strong background in machine learning, fullstack web development, data processing, and much more.
@@ -16,21 +16,32 @@ I am a determined and hardworking person who genuinely enjoys solving problems a
 * (class=green)(Fullstack Academy, Chicago)              September 2019 - December 2020
 
 (class=cyan bold)(Experience):
-* (class=green)(Caltech IMSS Rep)                        April 2022 - June 2023
+* (class=green)(Co-Founder & Lead Fullstack Developer @ ThoughtTree INC)           9/14/2024 - Now
+  Created a robust application API with FastAPI and MongoDB & designed responsive and interactive user interfaces in React.
+  Integrated ThoughtTree's custom PatientPrompt framework into our API alongside ML engineers.
+
+* (class=green)(Programmer Analyst 2 @ UNM)                                       7/28/2024 - Now
+  Maintained and developed systems/websites for university staff and students.
+
+* (class=green)(Fullstack Software Engineer @ Lumair)                             5/6/2024 - 8/20/2024
+  Designed application interface and functionality with React
+  Created & integration application with Python FastAPI Server.
+
+* (class=green)(Caltech IMSS Rep)                                                 April 2022 - June 2023
   Provided technical support to peers at Caltech and managed public house devices.
 
-* (class=green)(ML Teaching Assistant)                   September 2021 - March 2022
+* (class=green)(ML Teaching Assistant)                                            September 2021 - March 2022
   Taught CS 12: TensorFlow, a fast-paced class in machine learning for both undergraduate and graduate students.
 
-* (class=green)(Frontend Software Engineer Intern)       July 2020 - September 2020
+* (class=green)(Frontend Software Engineer Intern)                                July 2020 - September 2020
   Worked with a designer through Figma and developed frontend functionality for the [Zette chrome extension](https://www.zette.com/).
 
 (class=cyan bold)(What I Bring to the Table):
-  * Expertise in Python, TensorFlow2, C, C++, C#, Javascript, Node.js, React+Redux, Java, etc
+  * Expertise in Python, TensorFlow2, C, C++, C#, Javascript, Java, Ocaml, Haskell, R, MatLab, Node.js, React+Redux
   * Strong problem-solving skills and a knack for turning ideas into reality.
   * Experience in working on difficult projects in teams
 
-Reach out to me at [nick@jasinski3.com](mailto:nick@jasinski3.com)!
+Reach out to me at [contact@njasi.com](mailto:contact@njasi.com)!
 
 Just for fun, this website is a simple bash emulator.
   Coming soon: redirection and piping (might fully remake with proper AST parsing)
